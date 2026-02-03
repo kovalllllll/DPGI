faa5f3bc05be9dbfbd26225d7fd47b6fd0a4e368
Artem Koval <kovalartem9010@gmail.com>
2026-02-03 22:10:39 +0200

    Add XML documentation for Program class and Main method

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -1,7 +1,14 @@
 ﻿namespace lab1;
 
+/// <summary>
+/// Program contains the application's entry point.
+/// </summary>
 public static class Program
 {
+    /// <summary>
+    /// Application entry point.
+    /// </summary>
+    /// <param name="args">Command-line arguments passed to the application.</param>
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
